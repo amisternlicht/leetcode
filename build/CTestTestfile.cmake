5471b3116d22e0eafa90899fc871f62c5f2a207b
ami sternlicht <amisternlicht@gmail.com>
2022-10-31 18:28:01 +0200

    deleted build and vscode files

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/my/Desktop/tryMe/leetcode
-# Build directory: C:/Users/my/Desktop/tryMe/leetcode/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
